c48edfc56638de334b10611733916a097101ac59
amkpa <amitkumarpalai3@gmail.com>
2025-09-14 12:42:56 +0530

    Initial Modules Added

---
+++ Catalog Service/Catalog-Service/src/test/java/com/rmsa/catalog/Catalog/Service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmsa.catalog.Catalog.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Monitoring Service/Monitoring-Service/src/main/java/com/rmsa/monitoring/Monitoring/Service/MonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rmsa.monitoring.Monitoring.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
